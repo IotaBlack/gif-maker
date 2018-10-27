42080d1809b1c0936beaacd991d4de3c317e45a0
The_Circuit <kristofferomdal@gmail.com>
2018-10-28 00:21:40 +0200

    multple filter support

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 /**@typedef Options
  * @property {Number} wavelength
  * @property {Number} intensity
- * 
+ * @property {Number} offset
  */
+
+function options(wavelength = 1, intensity = 0, offset = 0) {
+    return { wavelength, intensity, offset }
+}
 
 
 /**
@@ -29,30 +33,81 @@ var currentframe = 0
 var preview = true
 /**@type {ImageData} */
 var previewImage = ctx.createImageData(100, 100)
+var imageFilters = {
+    /**@param {Options} options */
+    wave: function (imgd, options, offset = 0) {
+        var wavelength = options.wavelength
+        var intensity = options.intensity
+        var img = ctx.createImageData(imgd.width + intensity * 2, imgd.height)
+
+        offset += options.offset
+
+        for (let i = 0; i < img.width; i++) {
+            for (let j = 0; j < img.height; j++) {
+                var index1 = (j * img.width + i) * 4
+                img.data[index1 + 0] = background >> 16 & 0xff
+                img.data[index1 + 1] = background >> 8 & 0xff
+                img.data[index1 + 2] = background >> 0 & 0xff
+                img.data[index1 + 3] = 255
+            }
+        }
+
+        for (let i = 0; i < imgd.width; i++) {
+            for (let j = 0; j < imgd.height; j++) {
+                var index1 = (j * imgd.width + i) * 4
+                var index2 = (j * img.width + i +
+                    Math.floor(Math.sin((j / wavelength + offset) / Math.PI * 2) * intensity + intensity)) * 4
+
+                img.data[index2 + 0] = imgd.data[index1 + 0]
+                img.data[index2 + 1] = imgd.data[index1 + 1]
+                img.data[index2 + 2] = imgd.data[index1 + 2]
+                img.data[index2 + 3] = imgd.data[index1 + 3]
 
 
-load(document.getElementById('preview'))
+            }
+        }
+
+        return img
+    }
+}
+var filters = []
+
+function initialize() {
+
+    filters.push(new filter(imageFilters.wave, options(10, 10, 0)))
 
 
-animatePreview()
-window.addEventListener("dragover", function (e) { e.preventDefault(); }, true);
-window.addEventListener("drop", function (e) {
-    e.preventDefault();
-    log("load")
-    log(e)
-    loadfile(e.dataTransfer.files[0]);
-}, true);
+    load(document.getElementById('preview'))
 
 
-gifbutton.addEventListener('click',
-    e => {
-        createGif({
-            wavelength: parseFloat(wavelength.value),
-            intensity: parseFloat(intensity.value)
-        },
-            parseFloat(speed.value), delay)
-    })
+    animatePreview()
+    window.addEventListener("dragover", function (e) { e.preventDefault(); }, true);
+    window.addEventListener("drop", function (e) {
+        e.preventDefault();
+        log("load")
+        log(e)
+        loadfile(e.dataTransfer.files[0]);
+    }, true);
 
+
+    gifbutton.addEventListener('click',
+        e => {
+            createGif({
+                wavelength: parseFloat(wavelength.value),
+                intensity: parseFloat(intensity.value)
+            },
+                parseFloat(speed.value), delay)
+        })
+
+}
+function filter(method, options) {
+    this.method = method
+    this.options = options
+}
+
+filter.prototype.apply = function (img, offset) {
+    return this.method(img, this.options, offset)
+}
 
 function loadfile(src) {
     if (src === undefined) { } else {
@@ -102,50 +157,26 @@ function load(src) {
     drawPreview({ wavelength: 0, intensity: 0 }, 0)
 }
 
+function applyFilters(img, offset) {
+
+    for (let i = 0; i < filters.length; i++) {
+        img = filters[i].apply(img, offset)
+    }
+
+    return img
+}
+
 /**@param {Options} options */
 function drawPreview(options, offset = 0) {
 
-    var img = wave(previewImage, options, offset)
+    var img = applyFilters(previewImage, offset)
     displaycvs.width = img.width
     displaycvs.height = img.height
 
     displayctx.putImageData(img, 0, 0)
 }
 
-/**@param {Options} options */
-function wave(imgd, options, offset = 0) {
-    var wavelength = options.wavelength
-    var intensity = options.intensity
-    var img = ctx.createImageData(imgd.width + intensity * 2, imgd.height)
 
-
-    for (let i = 0; i < img.width; i++) {
-        for (let j = 0; j < img.height; j++) {
-            var index1 = (j * img.width + i) * 4
-            img.data[index1 + 0] = background >> 16 & 0xff
-            img.data[index1 + 1] = background >> 8 & 0xff
-            img.data[index1 + 2] = background >> 0 & 0xff
-            img.data[index1 + 3] = 255
-        }
-    }
-
-    for (let i = 0; i < imgd.width; i++) {
-        for (let j = 0; j < imgd.height; j++) {
-            var index1 = (j * imgd.width + i) * 4
-            var index2 = (j * img.width + i +
-                Math.floor(Math.sin((j / wavelength + offset) / Math.PI * 2) * intensity + intensity)) * 4
-
-            img.data[index2 + 0] = imgd.data[index1 + 0]
-            img.data[index2 + 1] = imgd.data[index1 + 1]
-            img.data[index2 + 2] = imgd.data[index1 + 2]
-            img.data[index2 + 3] = imgd.data[index1 + 3]
-
-
-        }
-    }
-
-    return img
-}
 
 function animatePreview(timer) {
     drawPreview({
@@ -163,7 +194,7 @@ function animatePreview(timer) {
 /**@param {Options} options */
 function createGif(options, speed, delay) {
     var step = speed
-    var img = wave(ctx.getImageData(0, 0, cvs.width, cvs.height), options, 0)
+    var img = applyFilters(ctx.getImageData(0, 0, cvs.width, cvs.height), 0)
     displaycvs.width = img.width
     displaycvs.height = img.height
 
@@ -196,7 +227,7 @@ function createGif(options, speed, delay) {
     preview = false
     currentframe = 0
     for (let i = 0; i < 10; i += step) {
-        img = wave(ctx.getImageData(0, 0, cvs.width, cvs.height), options, i)
+        img = applyFilters(ctx.getImageData(0, 0, cvs.width, cvs.height), i)
         //displayctx.putImageData(img, 0, 0)
         gifframes.push(img)
         gif.addFrame(img, { delay: delay, copy: true })
@@ -223,3 +254,6 @@ function setBackground(r, g, b) {
 
     ctx.putImageData(data, 0, 0)
 }
+
+
+initialize()
